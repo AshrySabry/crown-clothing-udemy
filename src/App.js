15cad062e2308cc77eda206cba79874f74e77411
AshrySabry <ashrysabry@gmail.com>
2021-03-17 00:44:35 +0800

    Update our menu items with withRouterHOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import {Switch, Route} from 'react-router-dom' ;
 
-import HomePage from './pages/homepage/homepage.component'
+
+import HomePage from './pages/homepage/homepage.component';
+
+const Hatspage = () => (
+  <div>
+    <h1>
+      HATS PAGE
+    </h1>
+  </div>
+)
 
 function App() {
   return (
-    <HomePage>
-      
-    </HomePage>
+    <Switch>
+      <div>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={Hatspage}/>
+    </div>
+    </Switch>
+    
   )
 }
 
