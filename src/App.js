568312a3b0bb270dca26a30746d141bdad2e5480
AshrySabry <ashrysabry@gmail.com>
2021-04-03 22:51:00 +0800

    Added Sign In Page and Add Shop Page

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import {Switch, Route} from 'react-router-dom' ;
 
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-const Hatspage = () => (
-  <div>
-    <h1>
-      HATS PAGE
-    </h1>
-  </div>
-)
 
 function App() {
   return (
-    <Switch>
-      <div>
+    <div>
+      <Header />
+      <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/hats' component={Hatspage}/>
-    </div>
+        <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInSignUpPage}/>
     </Switch>
+    </div>
+    
     
   )
 }
